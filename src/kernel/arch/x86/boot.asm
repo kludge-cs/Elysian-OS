2992176b6f86aaa24410a4df5f67752a66793429
Ender <neverwas@protonmail.com>
2018-09-04 15:00:33 -0700

    Yeah I'm still stuck

--- src/kernel/arch/x86/boot.asm
+++ src/kernel/arch/x86/boot.asm
@@ -10,15 +10,13 @@ VIDEO_USE_TEXT equ 1
 VIDEO_HEIGHT   equ 115 ;chars here
 VIDEO_WIDTH    equ 58
 
-KERNEL_PD_INDEX  equ VIRT_BASE >> 22
-
 PD_PRESENT      equ 00000001b
 PD_READWRITE    equ 00000010b ;Read & Write
 PD_USER         equ 00000100b ;if not set, only supervisor can access
 PD_WRITETHROUGH equ 00001000b ;writethrough if enabled, writeback if not
 PD_NOCACHE      equ 00010000b ;disable caching if set
 PD_ACCESSED     equ 00100000b ;should set set if page has been accessed
-PT_ZERO         equ 01000000b
+;this missing bit is always zero
 PD_SIZE         equ 10000000b ;4kib vs 4mib
 
 PT_PRESENT      equ 000000001b
@@ -31,16 +29,11 @@ PT_DIRTY        equ 001000000b ;has it been written to
 PT_PAT          equ 010000000b ;fuck intel
 PT_GLOBAL       equ 100000000b ;prevents the TLB from updating the address in its cache if CR3 is reset
 
-PAGE_SIZE  equ 0x1000
+PAGE_SIZE equ 0x1000
 HUGE_SIZE equ 0x400000
 
-VIRT_BASE equ  0xC0000000 ;rodata, text
+VIRT_BASE equ 0xC0000000
 PD_INDEX  equ VIRT_BASE >> 22
-
-
-;TODO: SUPPORT FUCKING INTELS FUCKING 4 LEVEL OR PAE BULLSHIT
-;TODO: figure out what PAT is
-
 
 section .multiboot
 align 4
@@ -67,20 +60,19 @@ mboot_header:
 	dd 32 ;color depth, ignored in text mode
 
 section .data
-align 0x1000
 global mboot_magic_check
 global mboot_info_struct
 multiboot_magic_check: dd 0
 multiboot_info: dd 0
-
+align 0x1000
 page_dir: resd 1024
 page_table: resd 1024
 
 section .text
 global _start
 extern kbegin
-extern rw_end
 extern ro_end
+extern rw_end
 _start:
 	cmp eax, 0x2BADB002
 	jne hang
@@ -88,34 +80,40 @@ _start:
 	add ebx, VIRT_BASE ;TODO: Check if this is even under the kernel
 	mov dword [multiboot_info], ebx
 
+;Start with paging stuff:
 	lea eax, [page_table - VIRT_BASE]
+	
 	mov ebx, PT_PRESENT
 
+	mov ecx, (ro_end - VIRT_BASE)
+	shr ecx, 12
+
 .fill_ro:
-	mov [eax], ebx ;insert entry
+	mov dword [eax], ebx ;insert entry
 	add eax, 4 ;next entry
 	add ebx, PAGE_SIZE ;next mem address
-	cmp ebx, (ro_end - VIRT_BASE)
-	jl .fill_ro
+	loop .fill_ro
+;;
 
 	or ebx, PT_READWRITE
+	mov ecx, (rw_end - VIRT_BASE)
+	sub ecx, (ro_end - VIRT_BASE) ;subtract what we already did
+	shr ecx, 12
 .fill_rw:
-	mov [eax], ebx
+	mov dword [eax], ebx
 	add eax, 4
 	add ebx, PAGE_SIZE
-	cmp ebx, (rw_end - VIRT_BASE)
-	jl .fill_rw
+	loop .fill_rw
+;;
 
-;done with page table
-	lea ebx, [page_table - VIRT_BASE] ;load page table
-	or ebx, PD_PRESENT | PD_READWRITE ;set flags
+;done with PT, fill PD
+	lea ebx, [page_table - VIRT_BASE]
+	or ebx, PD_PRESENT | PD_READWRITE ;set flags for PD entry
 
-	lea eax, [page_dir - VIRT_BASE] ;load page dir
-	mov dword [eax], ebx ;move page table to 0 in page dir
-	add eax, PD_INDEX ;move to higher half entyr
-	mov dword [eax], ebx ;move page table to higher half entry
+	mov dword [page_dir - VIRT_BASE], ebx
+	mov dword [page_dir - VIRT_BASE + PD_INDEX], ebx ;move page table to higher half entry
 
-	xchg bx, bx ;bochs breakpoint
+;actually enable paging and stuff
 	lea eax, [page_dir - VIRT_BASE]
 	mov cr3, eax
 
