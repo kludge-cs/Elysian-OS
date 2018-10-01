07156217863cc5b4f17e099c903cc57fdfad8e3a
Ender <neverwas@protonmail.com>
2018-10-01 01:08:23 -0700

    let's not confuse people here...

--- src/kernel/libk/include/stdarg.h
+++ src/kernel/libk/include/stdarg.h
@@ -1,14 +1,8 @@
-/* This is free and unencumbered software released into the public domain. */
+#pragma once
 
-#ifndef _STDARG_H
-#define _STDARG_H
-
-/**
- * @file
- *
- * <stdarg.h> - Variable arguments.
- *
- * @see http://libc11.org/stdarg/
+/*
+ * Taken from libc11
+ * see http://libc11.org/stdarg/
  */
 
 typedef __builtin_va_list va_list;
@@ -20,5 +14,3 @@ typedef __builtin_va_list va_list;
 #define va_end(ap)          __builtin_va_end((ap))
 
 #define va_copy(dest, src)  __builtin_va_copy((dest), (src))
-
-#endif /* _STDARG_H */
