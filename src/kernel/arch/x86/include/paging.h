916ccebbca09dddc09f30c6ec33496de839e2f79
Ender <neverwas@protonmail.com>
2018-10-09 19:30:00 -0700

    meh

---
+++ src/kernel/arch/x86/include/paging.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <types.h>
+
+struct pt_entry_s
+{
+	unsigned present  : 1;
+	unsigned rw       : 1;
+	unsigned user     : 1;
+	unsigned wthrough : 1;
+	unsigned nocache  : 1;
+	unsigned accessed : 1;
+	unsigned dirty    : 1;
+	unsigned zero     : 2; /* actually [PAT, global] but will be zero anyway */
+	unsigned avail    : 2;
+	unsigned addrress : 20;
+} __attribute__ ((packed));
+
+struct pd_entry_s
+{
+	unsigned present  : 1;
+	unsigned rw       : 1;
+	unsigned user     : 1;
+	unsigned wthrough : 1;
+	unsigned nocache  : 1;
+	unsigned accessed : 1;
+	unsigned dirty    : 1;
+	unsigned zero     : 2; /* Actually [zero, global], but global is ignored */
+	unsigned avail    : 2;
+	unsigned addrress : 20;
+} __attribute__ ((packed));
+
+struct pt_entry_s typedef page_table_t[1024];
+struct pd_entry_s typedef page_dir_t[1024];
+
+extern page_dir_t page_dir;
+
+int8 map_page_large (void * virt, struct pd_entry_s pd_entry);
--- src/kernel/arch/x86/paging.c
+++ src/kernel/arch/x86/paging.c
@@ -1,34 +1,15 @@
+#include <paging.h>
+#include <types.h>
 #include <vmem.h>
 
-struct pt_entry_s
+#define TLB_FLUSH(addr) __asm__ volatile ("invlpg (%0)" :: "r" (virt) : "memory")
+
+int8 map_page_large (void * virt, struct pd_entry_s pd_entry)
 {
-	unsigned present  : 1;
-	unsigned rw       : 1;
-	unsigned user     : 1;
-	unsigned wthrough : 1;
-	unsigned nocache  : 1;
-	unsigned accessed : 1;
-	unsigned dirty    : 1;
-	unsigned zero     : 2; /* actually [PAT, global] but will be zero anyway */
-	unsigned avail    : 2;
-	unsigned addrress : 20;
-} __attribute__ ((packed));
+	if (page_dir[(unsigned long)virt >> 22].present)
+		return -1;
 
-struct pd_entry_s
-{
-	unsigned present  : 1;
-	unsigned rw       : 1;
-	unsigned user     : 1;
-	unsigned wthrough : 1;
-	unsigned nocache  : 1;
-	unsigned accessed : 1;
-	unsigned dirty    : 1;
-	unsigned zero     : 2; /* Actually [zero, global], but global is ignored */
-	unsigned avail    : 2;
-	unsigned addrress : 20;
-} __attribute__ ((packed));
-
-struct pt_entry_s typedef page_table_t[1024];
-struct pd_entry_s typedef page_dir_t[1024];
-
-extern page_dir_t * page_dir;
+	page_dir[(unsigned long)virt >> 22] = pd_entry;
+	TLB_FLUSH(virt);
+	return 0;
+}
